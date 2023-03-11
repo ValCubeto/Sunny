c5b4c85cf6c1da7fdd150196cd56d12cf27736a0
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2023-03-10 23:43:43 -0300

    .

--- 4/src/main.rs
+++ 4/src/main.rs
@@ -1,48 +1,71 @@
-use std::env::{args_os, ArgsOs};
-use std::ffi::OsString;
 use std::process::ExitCode;
-use std::{fs, path};
+use std::env;
+use std::path::Path;
+use std::fs;
 use std::collections::HashMap;
 
+const EXTENSION: &str = ".sny";
+
 fn main() -> ExitCode {
-	let args: ArgsOs = args_os();
-	if args.len() == 0 {
+	let mut args: Vec<String> = env::args().collect();
+	if args.len() < 2 {
 		eprintln!("Missing arguments");
 		return ExitCode::FAILURE;
 	}
-	let mut flags: Vec<OsString> = Vec::new();
-	let mut i = 0;
-	let mut last_flag_index = 0;
+	let executor_path: String = args.remove(0);
+	let mut flags: Vec<String> = Vec::new();
 
-	for arg in args.into_iter() {
-		if arg.len() < 1 || arg[0] != "-" {
-			last_flag_index = i;
+	for (i, arg) in args.clone().iter().enumerate() {
+		if arg.len() < 1 || arg.chars().nth(0) != Some('-') /* || arg == "--" /* end of flags */ */ {
 			break;
 		}
-		flags.push(arg.clone());
-		i += 1;
+		let flag: String = args.remove(i);
+		flags.push(flag);
 	}
+	let mut path_arg: String = args.remove(0);
 
-	let exec_args = args.section(last_flag_index, args.len() - 1);
-	let file_relative_path = args[args.len() - 1];
-	let mut file_path = realpath(file_relative_path); // add the extension if necesary
+	dbg!(&executor_path);
+	dbg!(&flags);
+	dbg!(&path_arg);
+	dbg!(&args);
 
-	// check if exists
-	// check is a file
-
-	let file: fs::File = match fs::File::open(file_path) {
-		Ok(file) => file,
-		Err(error) => {
-			eprintln!("Failed to read \"{file_relative_path}\": {error}");
-			return ExitCode::FAILURE;
-		}
+	let code = match try_read([&path_arg, &(path_arg.clone() + EXTENSION)]) {
+		Ok(data) => data,
+		Err(exit_code) => return exit_code
 	};
 
+	/*
 	let mut tokens: Vec<Token> = Vec::new();
-	tokens.push(Token { text: String::from("hello"), token_type: TokenType::Word });
+	tokens.push(Token {
+		text: String::from("hello"),
+		token_type: TokenType::Word
+	});
 
 	println!("{:#?}", tokens);
+	*/
 	return ExitCode::SUCCESS;
+}
+
+fn try_read(paths: [&String; 2]) -> Result<String, ExitCode> {
+	for path in paths.clone() {
+		if !Path::new(&path).exists() {
+			continue;
+		}
+		if !Path::new(&path).is_file() {
+			eprintln!("\"{}\" is not a file", path);
+			return Err(ExitCode::FAILURE);
+		}
+		let file: String = match fs::read_to_string(&path) {
+			Ok(data) => data,
+			Err(error) => {
+				eprintln!("Failed to read \"{}\": {}", path, error.kind());
+				return Err(ExitCode::FAILURE);
+			}
+		};
+		return Ok(file);
+	}
+	eprintln!("File \"{}\" not found", paths[0]);
+	return Err(ExitCode::FAILURE);
 }
 
 #[derive(Debug)]
@@ -59,6 +82,6 @@ struct Token {
 
 #[derive(Debug)]
 struct Flag {
-	name: OsString,
-	value: Option<OsString> // OsString | None
+	name: String,
+	value: Option<String> // OsString | None
 }
