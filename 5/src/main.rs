db4638b4e651d94a219e3bd2e9bc38802d4129f5
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2023-05-10 15:57:17 -0300

    .

--- 5/src/main.rs
+++ 5/src/main.rs
@@ -1,10 +1,10 @@
-mod parsed_args;
+mod parse_args;
 mod info;
 mod console;
 
 fn main() {
 	#[allow(unused)]
-	let (executor_path, flags, file_path, args): _ = parsed_args::new();
+	let (executor_path, flags, file_path, args): _ = parse_args::new();
 
 	let size: crossterm::Result<(u16, u16)> = crossterm::terminal::size();
 
