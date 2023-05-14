8762677ff575f2fd90dafc289c74a2a38ae3d537
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2023-05-13 22:13:01 -0300

    .

--- 5/src/files.rs
+++ 5/src/files.rs
@@ -2,9 +2,9 @@ use std::fs::read_to_string;
 use crate::errors::LoadError;
 
 pub fn read(path: String) -> String {
-	match read_to_string(path) {
+	match read_to_string(&path) {
 		Err(err) => {
-			LoadError!("{}", err);
+			LoadError!("failed to read \"{}\", {}", path, err);
 		}
 		Ok(file) => file
 	}
