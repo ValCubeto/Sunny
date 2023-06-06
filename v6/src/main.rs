416f5e3b901eeb9ede8508d00d37a97f1c94701b
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2023-06-06 16:11:16 -0300

    .

--- v6/src/main.rs
+++ v6/src/main.rs
@@ -4,10 +4,18 @@ fn main() {
 	#[allow(unused)]
 	let (exec_path, flags, main_path, args) = parse_args();
 
-	// let file = read_file(main_path);
-	// let global_object = Object::from([ ("process".into(), Object::from([ ("exec_path".into(), exec_path.into()) ])) ]);
+	// let global_object = Object::from([
+	//   ("process".into(), Object::from([
+	//     ("exec_path".into(), exec_path.into())
+	//   ]))
+	// ]);
+
+	// let (file, main_path) = read_file(main_path);
+
 	// let main_module = parse_module(file);
+
 	// let arguments = Arguments::from([("0", args.into())]);
+
 	// main_module.exec_function("main".into(), arguments, global_object);
 }
 
