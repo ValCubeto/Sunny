1abf1f7fc5fc4dca736a807a7ac6463786923953
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2023-07-04 04:19:43 -0300

    .

--- v6/src/main.rs
+++ v6/src/main.rs
@@ -5,23 +5,26 @@ use crate::{
 };
 
 fn main() {
-	#[allow(unused)]
 	let (exec_path, flags, main_path, args) = arg_parser::parse_args();
 
-	#[allow(unused)]
 	let (code, main_path) = files::read_file(main_path);
-	let ctx = &mut Context::new(main_path, code);
+	let ctx = &mut Context::new(main_path.clone(), code);
 
 	#[allow(unused)]
 	let global_object = Dict::from([
-		(Key::from("hello"), Value::String(String::from("sexo")))
+		(Key::from("process"), Value::Dict(Dict::from([
+			(Key::from("exec_path"), Value::String(exec_path)),
+			(Key::from("flags"), Value::List(flags.iter().map(|v| Value::String(v.clone())).collect())),
+			(Key::from("main_path"), Value::String(main_path)),
+			(Key::from("args"), Value::List(args.iter().map(|v| Value::String(v.clone())).collect())),
+		])))
 	]);
 
 	let main_module = ns_parser::parse_namespace(ctx);
 	
+	dbg!(&main_module);
 
 	// let arguments = Arguments::from([(0, args.into())]);
-
 	// main_module.exec_function("main".into(), arguments, global_object);
 }
 
