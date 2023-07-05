bd4448fdbc74ecdbacb9980e7188ef9d27acfdea
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2023-07-05 17:39:09 -0300

    .

--- v6/src/types.rs
+++ v6/src/types.rs
@@ -5,14 +5,33 @@ use crate::{
 	params::Index,
 };
 
+#[allow(non_camel_case_types)]
 #[derive(Debug)]
 pub enum Value {
 	None,
 	String(String),
+
+	i8(i8),
+	i16(i16),
+	i32(i32),
+	i64(i64),
+	i128(i128),
+	isize(isize),
+	u8(u8),
+	u16(u16),
+	u32(u32),
+	u64(u64),
+	u128(u128),
+	usize(usize),
+	f32(f32),
+	f64(f64),
+
 	List(Vec<Value>),
 	Dict(Dict),
 	Namespace(Namespace),
-	Function(Function)
+	Function(Function),
+	// Struct(Struct),
+	Instance(Key, Vec<Value>),
 }
 
 #[derive(Debug)]
