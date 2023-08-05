1cb248062b6f04c923150095e72210d30c0cfa9b
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2023-08-05 00:37:08 -0300

    .

--- v7/src/global.rs
+++ v7/src/global.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 
-use crate::{id::Id, values::Value, hashmap};
+use crate::{id::Id, values::Value, hashmap, functions::FunctionValue};
 
 pub fn make_global() -> HashMap<Id, Value> {
 	hashmap! {
-		"println" => Value::Function()
+		"println" => Value::Function(Box::new(FunctionValue::Builtin(|args| { Value::None })))
 	}
 }
