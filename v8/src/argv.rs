28626cdd7d628bdb14ba7393ca85d5d6839d8f95
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2023-08-08 12:35:11 -0300

    .

---
+++ v8/src/argv.rs
@@ -0,0 +1,73 @@
+use crate::{
+	about::{ NAME, VERSION },
+	internal_error, argument_error,
+	id::Id
+};
+use std::{ process::exit, path::PathBuf };
+
+#[derive(Debug)]
+pub struct ParsedArgs {
+	pub exec_path: PathBuf,
+	pub flags: Vec<Id>,
+	pub main_path: PathBuf,
+	pub args: Vec<Id>
+}
+
+pub fn parse_args() -> ParsedArgs {
+	let mut raw_args = std::env::args_os().enumerate();
+
+	let exec_path: PathBuf = match raw_args.next() {
+		Some((_i, os_string)) => PathBuf::from(os_string),
+		None => InternalError!("argv is empty")
+	};
+
+	let mut flags: Vec<Id> = Vec::new();
+
+	let mut main_path = None;
+
+	for (i, raw_arg) in &mut raw_args {
+		let flag: &str = match raw_arg.to_str() {
+			Some(flag) => flag,
+			None => argument_error!("the argument at position {} has some invalid unicode", i + 1)
+		};
+
+		if !flag.starts_with('-') {
+			main_path = Some(PathBuf::from(raw_arg));
+			break;
+		}
+
+		match flag {
+			"-v" | "--version" => {
+				let arg_count = raw_args.len();
+				if arg_count > 0 {
+					println!("# unused {} extra arguments", arg_count);
+				}
+				println!("{NAME} {VERSION}");
+				exit(0);
+			}
+			"--test" => flags.push(Id::from(flag)),
+			_ => argument_error!("invalid flag {:?}", flag)
+		}
+	}
+
+	if main_path.is_none() {
+		// TODO: interactive mode
+		argument_error!("missing file path");
+	}
+
+	let mut args: Vec<Id> = Vec::new();
+
+	for (i, arg) in &mut raw_args {
+		args.push(match arg.to_str() {
+			Some(arg) => Id::from(arg),
+			None => argument_error!("the argument at position {} has some invalid unicode", i + 1)
+		})
+	}
+
+	ParsedArgs {
+		exec_path,
+		flags,
+		main_path: main_path.unwrap(), // unwrap_or(PathBuf::new())
+		args
+	}
+}
--- v8/src/main.rs
+++ v8/src/main.rs
@@ -2,5 +2,5 @@ fn main() {
 	println!("Hello, world!");
 }
 
-#[cfg(test)]
+mod argv;
 mod tests;
