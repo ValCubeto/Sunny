9e9fb7d42ded3910afe9ab47e7df6f55c0de8cc8
Val Cubeta <75044466+ValCubeta@users.noreply.github.com>
2023-10-06 08:08:07 -0300

    arturo

--- v9/src/repl/mod.rs
+++ v9/src/repl/mod.rs
@@ -1,12 +1,27 @@
-use std::io::{ stdin, Write as _ };
+use std::io::{ stdout, stdin, Write as _ };
+use crate::about::NAME;
 
 #[allow(unused)]
 pub fn main() {
   loop {
-    let input = stdin()
-      .read_line();
-    let line = input.trim();
+    print!("{}> ", NAME);
+    // otherwise the program reads the line and THEN prints the prompt
+    stdout()
+      .flush()
+      .expect("failed to print the prompt");
+
+    let mut buf: String = String::new();
+    stdin()
+      .read_line(&mut buf)
+      .expect("failed to read the line");
+    let line: &str = buf.trim();
+    drop(buf); // no longer needed
+
+    if line.is_empty() {
+      continue;
+    }
+
     debug!(line);
+    todo!();
   }
-  todo!();
 }
