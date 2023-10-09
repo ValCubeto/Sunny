5f3634e51cbcd95dec905ce394938482703c889a
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2023-10-09 19:08:57 -0300

    stuff

--- v9/src/table.rs
+++ v9/src/table.rs
@@ -1,32 +1,41 @@
+use std::ops::Div;
+
 use crate::debug;
 
-const HORIZONTAL: &str = "─";
-const VERTICAL: &str = "│";
+const HORIZONTAL: char = '─';
+const VERTICAL: char = '│';
 
-const LEFT_UP: &str = "┌";
-const RIGHT_UP: &str = "┐";
-const LEFT_DOWN: &str = "└";
-const RIGHT_DOWN: &str = "┘";
+const LEFT_UP: char = '┌';
+const RIGHT_UP: char = '┐';
+const LEFT_DOWN: char = '└';
+const RIGHT_DOWN: char = '┘';
 
-const HORIZONTAL_DOWN: &str = "┬";
-const HORIZONTAL_UP: &str = "┴";
-const CROSSED: &str = "┼";
-const VERTICAL_RIGHT: &str = "├";
-const VERTICAL_LEFT: &str = "┤";
+const HORIZONTAL_DOWN: char = '┬';
+const HORIZONTAL_UP: char = '┴';
+const CROSSED: char = '┼';
+const VERTICAL_RIGHT: char = '├';
+const VERTICAL_LEFT: char = '┤';
 
 pub fn print_table<const C: usize, const R: usize>(titles: [&str; C], rows: [[&str; C]; R]) {
-  let table = String::from(RIGHT_UP);
+  let mut table = String::from(LEFT_UP);
 
   let mut max_lens: Vec<usize> = Vec::with_capacity(C);
+  for column in titles.iter() {
+    max_lens.push(column.len());
+  }
   for row in rows.iter() {
-    let mut max_len = row[0].len();
-    for (i, column) in row.iter().skip(1).enumerate() {
-      let len = column.len();
-      if len > max_len {
-        max_len = len;
+    for (i, column) in row.iter().enumerate() {
+      if column.len() > max_lens[i] {
+        max_lens[i] = column.len();
       }
     }
-    max_lens.push(max_len);
   }
-  debug!(max_lens);
+  let mut iter = max_lens.iter();
+  let len = iter.next().unwrap();
+  table.push_str(HORIZONTAL.to_string().repeat(len + 2).as_str());
+  for column_len in iter {
+    table.push(HORIZONTAL_DOWN);
+    table.push_str(HORIZONTAL.to_string().repeat(column_len + 2).as_str());
+  }
+  debug!(table);
 }
