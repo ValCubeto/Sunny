e060ae97bfdaf001a69178feb46562475fb918a5
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2023-10-13 00:13:14 -0300

    .

--- v9/src/main.rs
+++ v9/src/main.rs
@@ -1,3 +1,4 @@
+extern crate rand;
 extern crate num_bigint;
 extern crate bigdecimal;
 
@@ -11,19 +12,11 @@ mod args;
 mod commands;
 mod table;
 
-use std::str::FromStr as _;
 use args::parse_args;
-use bigdecimal::BigDecimal;
-use num_bigint::BigInt;
+use rand::Rng;
 
 pub fn main() {
-  let a = &BigDecimal::from_str("0.1").unwrap();
-  let b = &BigDecimal::from_str("0.2").unwrap();
-  let c = &BigDecimal::from_str("0.3").unwrap();
-  println!("{} + {} = {}; {} == {} = {}",
-    a, b, a + b,
-    a + b, c, a + b == *c
-  );
-  println!("{}", BigInt::from_str(i64 ::MAX.to_string().as_str()).unwrap() + BigInt::from_str("100000").unwrap());
+  let mut thread = rand::thread_rng();
+  debug!(thread.gen_range(0..=100));
   parse_args();
 }
