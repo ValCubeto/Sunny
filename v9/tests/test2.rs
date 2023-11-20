2ccc66ce056487c3e2552b33777dff6484a12db2
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2023-11-19 22:05:55 -0300

    ,

--- v9/tests/test2.rs
+++ v9/tests/test2.rs
@@ -7,76 +7,83 @@ use hashbrown::HashMap;
 
 enum Value {
   None,
+  Struct(StructPtr),
   Uint8(u8)
 }
-type Map<T> = HashMap<Id, T>;
-type StructRef = Rc<Struct>;
-type Id = Rc<str>;
-type PropertyMap = BinTreeMap<Id, Property>;
+type Map<T> = HashMap<StringPtr, T>;
+type StructPtr = Rc<Struct>;
+type StringPtr = Rc<str>;
+type PropertyMap = BinTreeMap<StringPtr, Property>;
 
 struct Struct {
-  name: Id,
+  name: StringPtr,
   // sorted map, fast search
   props: PropertyMap
 }
 
 struct Property {
-  structure: StructRef,
+  structure: StructPtr,
   value: Value
 }
 
 struct Instance {
-  structure: StructRef,
+  structure: StructPtr,
   props: Vec<Property>
 }
 
 trait CreateInstance {
-  fn new_instance(&self, props: &Map<Property>) -> Instance;
+  fn new_instance(&self, props: Map<Property>) -> Instance;
 }
-impl CreateInstance for StructRef {
-  fn new_instance(&self, props: &Map<Property>) -> Instance {
+impl CreateInstance for StructPtr {
+  fn new_instance(&self, props: Map<Property>) -> Instance {
     let mut props = Vec::with_capacity(self.props.len());
-    props.push(Property { structure: StructRef::clone(self), value: Value::Uint8(8) });
+    for (key, value) in self.props.iter() {
+      value.value
+    }
+    props.push(Property { structure: StructPtr::clone(self), value: Value::Uint8(8) });
     Instance {
-      structure: StructRef::clone(self),
+      structure: StructPtr::clone(self),
       props
     }
   }
 }
 
-enum InstanceValue {
-  Props,
-  Value
+enum Variable {
+  Instance(Instance),
+  Value(Value)
 }
-type Stack = Map<>;
 
 fn main() {
-  let u8_struct = StructRef::new(Struct {
+  let u8_struct = StructPtr::new(Struct {
     name: "u8".into(),
     props: PropertyMap::new()
   });
 
   // where to store the number???????????
   let u8_instance = Instance {
-    structure: StructRef::clone(&u8_struct),
-    props: <Vec<Property>>::new()
+    structure: StructPtr::clone(&u8_struct),
+    props: Vec::new()
   };
 
   // struct Point { x: u8, y: u8 }
-  let point_struct = Rc::new(Struct {
+  let point_struct = StructPtr::new(Struct {
     name: "Point".into(),
     props: PropertyMap::from([
       ("x".into(), Property {
-        structure: StructRef::clone(&u8_struct),
+        structure: StructPtr::clone(&u8_struct),
         value: Value::None
       }),
       ("y".into(), Property {
-        structure: StructRef::clone(&u8_struct),
+        structure: StructPtr::clone(&u8_struct),
         value: Value::None
       }),
     ])
   });
 
-  let props = Map::from([]);
-  let instance = point_struct.new_instance(&props);
+  let point_instance = point_struct.new_instance(&Map::from([]));
+
+  let stack = Map::from([
+    ("Point".into(), Variable::Value(Value::Struct(point_struct))),
+    ("point".into(), Variable::Instance(point_instance))
+  ]);
 }
