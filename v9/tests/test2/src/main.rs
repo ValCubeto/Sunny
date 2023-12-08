b8e9c0ed2e1155e1cd57ad857b6a97d0be13b64b
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2023-12-08 20:08:20 -0300

    .

--- v9/tests/test2/src/main.rs
+++ v9/tests/test2/src/main.rs
@@ -1,21 +1,44 @@
 mod value;
-use std::{mem::{
-  size_of_val as size_by_val,
-  size_of
-}, rc::Rc};
+use std::{
+  mem::{
+    size_of_val as size_by_val,
+    // size_of
+  },
+  slice,
+  any::type_name
+};
 
 #[allow(clippy::size_of_ref)]
 fn main() {
-  let u8_val = 99_u8;
-  let usize_val = 105_usize;
-  let string = "hello";
+  let input = b"Hello";
+  // let len = size_by_val(&input);
+  let len = input.len();
+  let ptr = input.as_ptr() as usize;
+  let ptr_len = size_by_val(&ptr);
 
-  println!("0x{:x}: {} bytes | {} bits", ptr_as_usize(&u8_val), size_by_val(&&u8_val), size_by_val(&&u8_val) * 8);
-  println!("0x{:x}: {} bytes | {} bits", ptr_as_usize(&usize_val), size_by_val(&&usize_val), size_by_val(&&usize_val) * 8);
-  println!("0x{:x}: {} bytes | {} bits", ptr_as_usize(&string), size_of::<Rc<str>>(), size_of::<Rc<str>>() * 8);
-  println!("{} bits", size_by_val(&Box::new(1_usize)) * 8);
+  println!("Pointer value  : 0x{:X}", ptr);
+  println!("Size of pointer: {} bytes | {} bits", ptr_len, ptr_len * 8);
+  println!();
+  println!("Value of input : {:?}", input);
+  println!("Type of input  : {}", type_name_of(&input));
+  println!("Size of input  : {} bytes | {} bits", len, len * 8);
+  println!();
+  unsafe {
+    let bytes = slice::from_raw_parts(ptr as *const u8, len);
+    let max_index_len = bytes.len().to_string().len();
+    // Bytes reversed because of little-endian!
+    for (i, &byte) in bytes.iter().enumerate() {
+      println!("Byte #{:<0len$}: {:08b}", i + 1, byte, len = max_index_len);
+      println!("{:?}", byte as char);
+    }
+  };
 }
 
-pub fn ptr_as_usize<T>(ptr: &T) -> usize {
+pub fn ptr_to_first_byte<T>(ptr: &T) -> usize {
   ptr as *const T as usize
+}
+
+#[inline(always)]
+pub fn type_name_of<T>(_value: &T) -> &str {
+  type_name::<T>()
 }
