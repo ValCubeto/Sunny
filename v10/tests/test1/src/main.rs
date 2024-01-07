8d148126cb78021649daba9d8bb67694c4548f0c
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2024-01-07 01:23:43 -0300

    Dynamic imports

--- v10/tests/test1/src/main.rs
+++ v10/tests/test1/src/main.rs
@@ -1,17 +1,46 @@
+use std::{collections::HashMap, io::Write};
+
 use dlopen::raw::Library;
 
 fn main() {
-  let (_lib, hello) = load_func("hello");
-  println!("'hello' loaded");
-  hello();
-}
-
-fn load_func(name: &str) -> (Library, fn()) {
-  let lib = Library::open(format!("../../lib/bin/{}.dll", name))
-    .unwrap_or_else(|err| panic!("failed to open '{name}.dll': {err}"));
-  unsafe {
-    let func = lib.symbol(name)
-      .unwrap_or_else(|err| panic!("no '{name}' function: {err}"));
-    (lib, func)
+  let mut libs: HashMap<&str, Library> = HashMap::new();
+  let mut functions: HashMap<&str, fn()> = HashMap::new();
+  loop {
+    print!("> ");
+    std::io::stdout().flush().unwrap();
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).unwrap();
+    let line = line.trim();
+    match line {
+      "hello" => {
+        let lib = libs.entry("hello").or_insert_with(|| {
+          println!("Loading 'hello.dll'");
+          Library::open("../../lib/bin/hello.dll")
+            .unwrap_or_else(|err| panic!("failed to open 'hello.dll': {}", err))
+        });
+        let func = functions.entry("hello").or_insert_with(|| unsafe {
+          println!("Loading 'hello' from 'hello.dll'");
+          lib.symbol("hello")
+            .unwrap_or_else(|err| panic!("no function: {}", err))
+       });
+       func();
+      },
+      "bye" => {
+        let lib = libs.entry("bye").or_insert_with(|| {
+          println!("Loading 'bye.dll'");
+          Library::open("../../lib/bin/bye.dll")
+          .unwrap_or_else(|err| panic!("failed to open 'bye.dll': {}", err))
+        });
+        let func = functions.entry("bye").or_insert_with(|| unsafe {
+          println!("Loading 'bye' from 'bye.dll'");
+          lib.symbol("bye")
+            .unwrap_or_else(|err| panic!("no function: {}", err))
+       });
+       func();
+      },
+      _ => continue
+    }
+    println!("libraries: {:?}", libs.keys().collect::<Vec<_>>());
+    println!("libraries: {:?}", functions.keys().collect::<Vec<_>>());
   }
 }
