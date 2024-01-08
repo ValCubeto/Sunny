3ae4aa42a431b03bfdefb35af2189b3e179f101b
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2024-01-08 03:48:53 -0300

    .

--- v10/tests/test2/src/main.rs
+++ v10/tests/test2/src/main.rs
@@ -1,3 +1,38 @@
+use std::*;
+
 fn main() {
-  println!("Hello, world!");
+  // make the ast parser based on a list of tokens
+  let tokens = [
+    Token::Number(1),
+    Token::Add,
+    Token::Number(2),
+    Token::Mul,
+    Token::Number(3),
+  ];
+  let expected = Op::Add(
+      Op::Number(1).ptr(),
+      Op::Mul(
+          Op::Number(2).ptr(),
+          Op::Number(3).ptr()
+      ).ptr()
+  );
+}
+
+pub enum Token {
+  Number(i32),
+  Add,
+  Mul
+}
+
+pub enum Op {
+  Number(i32),
+  Add(Box<Op>, Box<Op>),
+  Mul(Box<Op>, Box<Op>),
+}
+impl Op {
+  /// I hate seeing `Box::new` everywhere
+  #[inline(always)]
+  pub fn ptr(self) -> Box<Self> {
+    Box::new(self)
+  }
 }
