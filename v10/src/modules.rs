82bc4eebf75eadbbccb796b61c0a6bc567015f95
ValentineBucket <75044466+ValentineBucket@users.noreply.github.com>
2024-01-21 14:49:06 -0300

    help

--- v10/src/modules.rs
+++ v10/src/modules.rs
@@ -1,10 +1,35 @@
-use crate::context::Context;
+use std::{str::Chars, iter::Enumerate};
 
-pub fn parse_module(code: &str) -> Context {
-  let mut ctx = Context::new(code);
-  while ctx.cursor() < ctx.char_count {
-    let word = ctx.expect_word();
-    todo!("{word:?}");
+
+#[allow(unused_labels, clippy::while_let_on_iterator)]
+pub fn parse_module(code: &str) {
+  let mut parser = ModuleParser::new(code);
+  'a: while let Some((i, ch)) = parser.try_next_char() {
+    if 
+    println!("({i}, {ch:?})");
   }
-  ctx
+}
+
+pub struct ModuleParser<'a> {
+  chars: Enumerate<Chars<'a>>
+}
+impl<'a> ModuleParser<'a> {
+  #[inline]
+  pub fn new(code: &'a str) -> ModuleParser<'a> {
+    ModuleParser {
+      chars: code.chars().enumerate()
+    }
+  }
+  #[inline]
+  pub fn try_next_char(&mut self) -> Option<(usize, char)> {
+    self.chars.next()
+  }
+
+  #[inline]
+  pub fn next_char(&mut self) -> (usize, char) {
+    match self.chars.next() {
+      None => syn_error!("unexpected end of input"),
+      Some(pair) => pair
+    }
+  }
 }
