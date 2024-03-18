35e5d28c1be0b32c02d394cfc8cb85607beef261
ValCubeta <valcubetaa@gmail.com>
2024-03-17 22:48:23 -0300

    todo

--- latest/v11/src/parse/parse_file.rs
+++ latest/v11/src/parse/parse_file.rs
@@ -8,9 +8,17 @@ pub fn parse_file(file_name: &str, data: &str) {
 
   println!("curr = {:?}", parser.current);
   match parser.current {
-    ch if ch.is_alphabetic() => {
+    ch if ch.is_ascii_alphabetic() => {
+      // TODO: match only specific identifiers, not any word
       let word = parser.parse_word();
-      syn_err!("unexpected identifier {word:?} here")
+      match word.as_str() {
+        "const" => {
+          parser.skip_whitespaces();
+          // let ident = parser.expect_word();
+          todo!()
+        },
+        _ident => syn_err!("unexpected identifier {word:?} here")
+      }
     },
     ch => syn_err!("invalid or unexpected token {ch:?}")
   }
