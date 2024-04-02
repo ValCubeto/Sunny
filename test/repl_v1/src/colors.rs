a27a15bf47b55775cf49a2a82ababe626d9b928b
ValCubeta <valcubetaa@gmail.com>
2024-04-02 19:42:52 -0300

    ?

--- test/repl_v1/src/colors.rs
+++ test/repl_v1/src/colors.rs
@@ -1,65 +1,68 @@
-const RESET: &str = "\x1b[0m";
+pub const RESET: &str = "\x1b[0m";
 
-const BOLD: &str = "\x1b[1m";
-const DIM: &str = "\x1b[2m";
+pub const BOLD: &str = "\x1b[1m";
+/// Set an specific color using 2;r;g;b syntax (maybe)
+pub const DIM: &str = "\x1b[2m";
 /// Works with dim too
-const BOLD_END: &str = "\x1b[22m";
+pub const BOLD_END: &str = "\x1b[22m";
 
-const ITALIC: &str = "\x1b[3, 23m";
-const ITALIC_END: &str = "\x1b[3, 23m";
+pub const ITALIC    : &str = "\x1b[3, 23m";
+pub const ITALIC_END: &str = "\x1b[3, 23m";
 
-const UNDERLINE       : &str = "\x1b[4m";
-const DOUBLE_UNDERLINE: &str = "\x1b[21m";
-const UNDERLINE_END   : &str = "\x1b[24m";
+pub const UNDERLINE       : &str = "\x1b[4m";
+pub const DOUBLE_UNDERLINE: &str = "\x1b[21m";
+pub const UNDERLINE_END   : &str = "\x1b[24m";
 
-const BLINK: &str = "\x1b[5m";
-const BLINK_END: &str = "\x1b[25m";
+pub const BLINK: &str = "\x1b[5m";
+pub const BLINK_END: &str = "\x1b[25m";
 
-const INVERSE: &str = "\x1b[7m";
-const INVERSE_END: &str = "\x1b[27m";
+pub const INVERSE: &str = "\x1b[7m";
+pub const INVERSE_END: &str = "\x1b[27m";
 
-const HIDDEN: &str = "\x1b[8m";
-const HIDDEN_END: &str = "\x1b[28m";
+pub const HIDDEN: &str = "\x1b[8m";
+pub const HIDDEN_END: &str = "\x1b[28m";
 
-const STRIKE_THROUGH: &str = "\x1b[9m";
-const STRIKE_THROUGH_END: &str = "\x1b[29m";
+pub const STRIKE_THROUGH: &str = "\x1b[9m";
+pub const STRIKE_THROUGH_END: &str = "\x1b[29m";
 
-const BLACK: &str = "\x1b[30, 39m";
-const RED: &str = "\x1b[31, 39m";
-const GREEN: &str = "\x1b[32, 39m";
-const YELLOW: &str = "\x1b[33, 39m";
-const BLUE: &str = "\x1b[34, 39m";
-const MAGENTA: &str = "\x1b[35, 39m";
-const CYAN: &str = "\x1b[36, 39m";
-const WHITE: &str = "\x1b[37, 39m";
-const GRAY: &str = "\x1b[90, 39m";
-const RED_BRIGHT: &str = "\x1b[91, 39m";
-const GREEN_BRIGHT: &str = "\x1b[92, 39m";
-const YELLOW_BRIGHT: &str = "\x1b[93, 39m";
-const BLUE_BRIGHT: &str = "\x1b[94, 39m";
-const MAGENTA_BRIGHT: &str = "\x1b[95, 39m";
-const CYAN_BRIGHT: &str = "\x1b[96, 39m";
-const WHITE_BRIGHT: &str = "\x1b[97, 39m";
+pub const BLACK: &str = "\x1b[30m";
+pub const RED: &str = "\x1b[31m";
+pub const GREEN: &str = "\x1b[32m";
+pub const YELLOW: &str = "\x1b[33m";
+pub const BLUE: &str = "\x1b[34m";
+pub const MAGENTA: &str = "\x1b[35m";
+pub const CYAN: &str = "\x1b[36m";
+pub const WHITE: &str = "\x1b[37m";
+pub const GRAY: &str = "\x1b[90m";
+pub const RED_BRIGHT: &str = "\x1b[91m";
+pub const GREEN_BRIGHT: &str = "\x1b[92m";
+pub const YELLOW_BRIGHT: &str = "\x1b[93m";
+pub const BLUE_BRIGHT: &str = "\x1b[94m";
+pub const MAGENTA_BRIGHT: &str = "\x1b[95m";
+pub const CYAN_BRIGHT: &str = "\x1b[96m";
+pub const WHITE_BRIGHT: &str = "\x1b[97m";
+pub const COLOR_END: &str = "\x1b[39m";
 
-const BG_BLACK: &str = "\x1b[40, 49m";
-const BG_RED: &str = "\x1b[41, 49m";
-const BG_GREEN: &str = "\x1b[42, 49m";
-const BG_YELLOW: &str = "\x1b[43, 49m";
-const BG_BLUE: &str = "\x1b[44, 49m";
-const BG_MAGENTA: &str = "\x1b[45, 49m";
-const BG_CYAN: &str = "\x1b[46, 49m";
-const BG_WHITE: &str = "\x1b[47, 49m";
-const BG_GRAY: &str = "\x1b[100, 49m";
-const BG_RED_BRIGHT: &str = "\x1b[101, 49m";
-const BG_GREEN_BRIGHT: &str = "\x1b[102, 49m";
-const BG_YELLOW_BRIGHT: &str = "\x1b[103, 49m";
-const BG_BLUE_BRIGHT: &str = "\x1b[104, 49m";
-const BG_MAGENTA_BRIGHT: &str = "\x1b[105, 49m";
-const BG_CYAN_BRIGHT: &str = "\x1b[106, 49m";
-const BG_WHITE_BRIGHT: &str = "\x1b[107, 49m";
+pub const BG_BLACK: &str = "\x1b[40m";
+pub const BG_RED: &str = "\x1b[41m";
+pub const BG_GREEN: &str = "\x1b[42m";
+pub const BG_YELLOW: &str = "\x1b[43m";
+pub const BG_BLUE: &str = "\x1b[44m";
+pub const BG_MAGENTA: &str = "\x1b[45m";
+pub const BG_CYAN: &str = "\x1b[46m";
+pub const BG_WHITE: &str = "\x1b[47m";
+pub const BG_GRAY: &str = "\x1b[100m";
+pub const BG_RED_BRIGHT: &str = "\x1b[101m";
+pub const BG_GREEN_BRIGHT: &str = "\x1b[102m";
+pub const BG_YELLOW_BRIGHT: &str = "\x1b[103m";
+pub const BG_BLUE_BRIGHT: &str = "\x1b[104m";
+pub const BG_MAGENTA_BRIGHT: &str = "\x1b[105m";
+pub const BG_CYAN_BRIGHT: &str = "\x1b[106m";
+pub const BG_WHITE_BRIGHT: &str = "\x1b[107m";
+pub const BG_END: &str = "\x1b[49m";
 
-const FRAMED: &str = "\x1b[51, 54m";
-const FRAMED_END: &str = "\x1b[51, 54m";
+pub const FRAMED: &str = "\x1b[51, 54m";
+pub const FRAMED_END: &str = "\x1b[51, 54m";
 
-const OVERLINED: &str = "\x1b[53, 55m";
-const OVERLINED_END: &str = "\x1b[53, 55m";
+pub const OVERLINED: &str = "\x1b[53, 55m";
+pub const OVERLINED_END: &str = "\x1b[53, 55m";
--- test/repl_v1/src/main.rs
+++ test/repl_v1/src/main.rs
@@ -1,18 +1,84 @@
+extern crate crossterm;
+extern crate lazy_static;
 
 #[allow(unused)]
 mod colors;
 
+use colors::*;
+use crossterm::{
+  event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyModifiers, MouseEventKind},
+  terminal::{self, Clear, ClearType}, ExecutableCommand,
+};
+use std::io::{self, Write};
+
+/// "Print and flush", but it's the same as C's
 macro_rules! printf {
   ($($arg: expr),*) => {{
-    use ::std::io::Write;
-    print!($($arg),*);
-    ::std::io::stdout().flush().unwrap();
+  use ::std::io::Write;
+  print!($($arg),*);
+  ::std::io::stdout().flush().unwrap();
   }};
 }
 
-fn main() {
+fn main() -> io::Result<()> {
+  let prompt = "> ";
+
   println!("Welcome to REPL v0.1!");
-  println!("Call {} for more information", "help()");
+  println!("Call {BOLD}help(){BOLD_END} for more information");
   println!();
-  printf!("> ");
+  printf!("{prompt}");
+
+  terminal::enable_raw_mode()?;
+  let mut stdout = io::stdout();
+  let stdin = io::stdin();
+  let mut input = String::new();
+  let mut in_multiline_mode = false;
+  let mut cursor = 0;
+
+  loop {
+    match event::read()? {
+      Event::Key(key_event) => match key_event.code {
+        KeyCode::Char(ch) => {
+          if ch == 'c' && key_event.modifiers.contains(KeyModifiers::CONTROL) {
+            stdout.execute(DisableMouseCapture)?;
+            break;
+          }
+          input.push(ch);
+        }
+        KeyCode::Enter => {
+          terminal::disable_raw_mode()?;
+          println!();
+          println!("input: {input:?}");
+          input = String::new();
+        }
+        KeyCode::Esc => {
+          stdout.execute(DisableMouseCapture)?;
+          println!();
+          println!();
+          println!("Goodbye!");
+          break;
+        },
+        KeyCode::Backspace => {
+          if !input.is_empty() {
+            let _removed = input.remove(cursor);
+          }
+        }
+        _ => {}
+      },
+      Event::Mouse(mouse_event) => match mouse_event.kind {
+        MouseEventKind::Down(mouse_event) => {},
+        MouseEventKind::Up(mouse_event) => {},
+        MouseEventKind::ScrollLeft => {},
+        MouseEventKind::ScrollRight => {},
+        MouseEventKind::Drag(button) => {},
+        MouseEventKind::Moved => {},
+        MouseEventKind::ScrollDown => {},
+        MouseEventKind::ScrollUp => {}
+      },
+      Event::Paste(text) => {},
+      Event::Resize(x, y) => {},
+      _ => {}
+    }
+  }
+  Ok(())
 }
