ed8f1be56995b3e8f131cd3d5d96a389072d801d
ValCubeta <valcubetaa@gmail.com>
2024-04-03 18:00:56 -0300

    Added cursor movements

--- test/repl_v1/src/main.rs
+++ test/repl_v1/src/main.rs
@@ -6,38 +6,42 @@ mod colors;
 
 use colors::*;
 use crossterm::{
-  cursor::MoveTo, event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind, KeyModifiers, MouseEventKind}, terminal::{self, Clear, ClearType}, ExecutableCommand
+  cursor::{ MoveLeft, MoveRight, MoveUp },
+  event::{
+    read as read_event,
+    DisableMouseCapture,
+    EnableMouseCapture,
+    Event,
+    KeyCode,
+    KeyEventKind,
+    KeyModifiers
+  },
+  terminal,
+  ExecutableCommand as _
 };
-use std::io::{self, Write};
-
-/// "Print and flush", but it's the same as C's
-macro_rules! printf {
-  ($($arg: expr),*) => {{
-  use ::std::io::Write;
-  print!($($arg),*);
-  ::std::io::stdout().flush().unwrap();
-  }};
-}
+use std::io;
 
 fn main() -> io::Result<()> {
   let prompt = "> ";
-  terminal::enable_raw_mode()?;
+  let mut stdout = io::stdout();
 
+  terminal::enable_raw_mode()?;
   println!("Welcome to REPL v0.1!");
   println!("Call {BOLD}help(){BOLD_END} for more information");
   println!();
   println!("{prompt}");
+  stdout.execute(MoveUp(1))?;
+  stdout.execute(MoveRight(prompt.len() as u16))?;
+  stdout.execute(EnableMouseCapture)?;
 
-  let mut stdout = io::stdout();
-  let stdin = io::stdin();
+  // let stdin = io::stdin();
   let mut input = String::new();
   let mut in_multiline_mode = false;
   let mut cursor = 0;
 
-  stdout.execute(EnableMouseCapture)?;
   loop {
     #[allow(clippy::single_match)]
-    match event::read()? {
+    match read_event()? {
       // NOTE: the event is triggered on both pressing and releasing,
       // so if not handled correctly it gets keys twice.
       Event::Key(key_event) if key_event.kind != KeyEventKind::Release => match key_event.code {
@@ -46,6 +50,7 @@ fn main() -> io::Result<()> {
             match ch {
               'c' => {
                 terminal::disable_raw_mode()?;
+                stdout.execute(DisableMouseCapture)?;
                 println!();
                 print!("Goodbye!");
                 break;
@@ -60,9 +65,11 @@ fn main() -> io::Result<()> {
                 // stdout.execute(EnableMouseCapture)?;
               }
             }
+            continue;
           }
           cursor += 1;
-          input.push(ch);
+          stdout.execute(MoveRight(1))?;
+          input.insert(cursor - 1, ch);
         }
         KeyCode::Enter => {
           terminal::disable_raw_mode()?;
@@ -71,25 +78,48 @@ fn main() -> io::Result<()> {
           println!("{prompt}");
           cursor = 0;
           input = String::new();
+          stdout.execute(MoveUp(1))?;
+          stdout.execute(MoveRight(prompt.len() as u16))?;
           terminal::enable_raw_mode()?;
         }
         KeyCode::Backspace => {
           if !input.is_empty() {
             let _removed = input.remove(cursor - 1);
             cursor -= 1;
+            stdout.execute(MoveLeft(1))?;
           }
         }
         KeyCode::Delete => {
-          //
+          if !input.is_empty() && cursor < input.len() {
+            let _removed = input.remove(cursor);
+            cursor -= 1;
+          }
         }
         KeyCode::Home => {
-          cursor = 0;
-          // stdout.execute(MoveTo(?, ?));
+          if cursor != 0 {
+            stdout.execute(MoveLeft(cursor as u16))?;
+            cursor = 0;
+          }
         }
         KeyCode::End => {
-          cursor = input.len() - 1;
-          // move real cursor
-          // stdout.execute(MoveTo(?, ?));
+          if cursor != input.len() {
+            stdout.execute(MoveRight((input.len() - cursor) as u16))?;
+            cursor = input.len();
+          }
+        }
+        KeyCode::Left => {
+          if cursor > 0 {
+            terminal::disable_raw_mode()?;
+            cursor -= 1;
+            stdout.execute(MoveLeft(1))?;
+            terminal::enable_raw_mode()?;
+          }
+        }
+        KeyCode::Right => {
+          if cursor < input.len() {
+            cursor += 1;
+            stdout.execute(MoveRight(1))?;
+          }
         }
         _ => {}
       },
