672d621d1a0fdd3f921781fe9261bcc00487d5fd
ValCubeta <valcubetaa@gmail.com>
2024-05-26 19:07:45 -0300

    .

--- latest/v12/src/main.rs
+++ latest/v12/src/main.rs
@@ -12,7 +12,7 @@ fn main() {
   #[allow(clippy::needless_borrow)]
   let mut parser = Parser::new(&file);
   parser.next_char();
-  // parse_expr(&mut parser);
+  parse_expr(&mut parser);
 }
 
 #[allow(unused)]
@@ -60,7 +60,7 @@ impl<'a> Parser<'a> {
     self.update_cursor_pos();
   }
 
-  /// Goes to the next character
+  /// Goes to the next character, ignoring the comments
   pub fn next_char(&mut self) {
     self.idx += 1;
     if self.current == '\0' {
@@ -103,7 +103,7 @@ impl<'a> Parser<'a> {
     println!("[idx={}] Called next_char and got: {:?}", self.idx, self.current);
   }
 
-  /// Skips the spaces until there is a token
+  /// Skips the spaces
   pub fn next_token(&mut self) {
     while Self::is_space(self.current) {
       self.next_char();
@@ -113,4 +113,49 @@ impl<'a> Parser<'a> {
 
 pub fn parse_expr(parser: &mut Parser) {
   parser.next_token();
+  println!("[idx={}] Parsing expr starting with: {:?}", parser.idx(), parser.current());
+  let val = parse_value(parser);
+  println!("{:?}", val);
+}
+
+pub fn parse_value(parser: &mut Parser) -> Expr {
+  parser.next_token();
+  match parser.current() {
+    '&' => {
+      parser.next_char();
+      Expr::Ref(parse_value(parser).ptr())
+    },
+    '*' => {
+      parser.next_char();
+      Expr::Deref(parse_value(parser).ptr())
+    },
+    '0'..='9' => {
+      let mut n = String::from(parser.current());
+      parser.next_char();
+      while parser.current().is_ascii_digit() {
+        n.push(parser.current());
+        parser.next_char();
+      }
+      Expr::Token(Value::Number(n))
+    },
+    _ => panic!("unexpected token {:?}", parser.current())
+  }
+}
+
+#[derive(Debug)]
+pub enum Value {
+  Number(String)
+}
+
+#[derive(Debug)]
+pub enum Expr {
+  Token(Value),
+  Ref(Box<Expr>),
+  Deref(Box<Expr>)
+}
+impl Expr {
+  #[inline(always)]
+  pub fn ptr(self) -> Box<Expr> {
+    Box::new(self)
+  }
 }
