2e73be95efe06cee2b9cec415de8f913c7441432
ValCubeta <valcubetaa@gmail.com>
2024-12-12 23:43:10 -0300

    ...

--- latest/v13/src/eval/parse/mod.rs
+++ latest/v13/src/eval/parse/mod.rs
@@ -4,16 +4,16 @@ pub mod expressions;
 pub mod values;
 pub mod types;
 use constants::parse_static;
-use items::Item;
+use items::Entity;
 use super::tokenize::tokens::Token as Tk;
 use super::tokenize::keywords::Keyword as Kw;
 
-pub fn parse(tokens: Vec<Tk>) -> Vec<Item> {
-  let items = Vec::new();
+pub fn parse(tokens: Vec<Tk>) -> Vec<Entity> {
+  let mut items = Vec::new();
   let mut tokens = tokens.iter();
   while let Some(token) = tokens.next() {
     let item = match token {
-      Tk::NewLine | Tk::Semicolon => {}
+      Tk::NewLine | Tk::Semicolon => continue,
       Tk::Keyword(Kw::Private) => todo!(),
       Tk::Keyword(Kw::Public) => todo!(),
       Tk::Keyword(Kw::Const) => parse_static(false, &mut tokens),
@@ -35,7 +35,8 @@ pub fn parse(tokens: Vec<Tk>) -> Vec<Item> {
       Tk::Keyword(keyword) => syntax_err!("unexpected keyword \"{keyword}\" here"),
       Tk::Ident(ident) => syntax_err!("unexpected identifier \"{ident}\" here"),
       _ => syntax_err!("unexpected token {token:?} here"),
-    }
+    };
+    items.push(item);
   }
   items
 }
